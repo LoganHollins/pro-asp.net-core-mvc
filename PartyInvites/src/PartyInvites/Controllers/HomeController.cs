0ed933b81ab9dbc25475745fb1fde2cd1e8a4c15
Logan <Logan.hollins1@gmail.com>
2017-02-16 14:21:13 -0800

    Initial commit

---
+++ PartyInvites/src/PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+using System.Linq;
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            var hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour > 12 ? "Good Afternoon" : "Good Morning";
+            return View("MyView");
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            Repository.AddResponse(guestResponse);
+            return View("Thanks", guestResponse);
+        }
+
+        [HttpGet]
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
+        }
+
+    }
+}
