52e4e5a2681f2b65f4da9c5e8ec8e92c4a3156f2
Logan Hollins <Logan.hollins1@gmail.com>
2017-03-09 16:31:27 -0800

    Add the ConfiguringApps Project

---
+++ ConfiguringApps/src/ConfiguringApps/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ConfiguringApps.Infrastructure;
+using Microsoft.Extensions.Logging;
+
+namespace ConfiguringApps.Controllers
+{
+    public class HomeController : Controller
+    {
+        private UptimeService uptime;
+        private ILogger logger;
+
+        public HomeController(UptimeService uptimeService, ILogger<HomeController> log)
+        {
+            uptime = uptimeService;
+            logger = log;
+        }
+
+        public IActionResult Index(bool throwException = false) {
+
+            if (throwException)
+            {
+                throw new System.NullReferenceException();
+            }
+
+            logger.LogDebug($"Handled {Request.Path} at uptime {uptime.Uptime}");
+            return  View(new Dictionary<string, string>
+            {
+                ["Message"] = "This is the Index action",
+                ["Uptime"] = $"{uptime.Uptime}ms"
+            });
+
+        }
+        
+        public ViewResult Error()
+        {
+            return View("Index", new Dictionary<string, string>
+            {
+                ["Message"] = "This is the Error action"
+            });
+        }
+    }
+}
---
+++ ConfiguringApps/src/ConfiguringApps/Startup.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using ConfiguringApps.Infrastructure;
+using Microsoft.Extensions.Configuration;
+
+namespace ConfiguringApps
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            Configuration = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile($"appsettings{env.EnvironmentName}.json")
+                .Build();
+        }
+        public IConfigurationRoot Configuration { get; set; }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().AddMvcOptions(options => { options.RespectBrowserAcceptHeader = true; });
+            services.AddSingleton<UptimeService>();
+        }
+
+        public void ConfigureDevelopmentServices(IServiceCollection services)
+        {
+            services.AddSingleton<UptimeService>();
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            app.UseExceptionHandler("/Home/Error");
+
+            app.UseStaticFiles();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                name: "default",
+               template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+
+        public void ConfigureDevelopment(IApplicationBuilder app, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug(LogLevel.Debug);
+
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseBrowserLink();
+            app.UseStaticFiles();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                name: "default",
+               template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
