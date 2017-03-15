b1109be4c0fce59fa6cff16b33666b11368ecf10
Logan Hollins <Logan.hollins1@gmail.com>
2017-03-15 11:22:38 -0700

    Add UrlAndRoutes Project

---
+++ UrlsAndRoutes/src/UrlsAndRoutes/Startup.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Routing;
+using UrlsAndRoutes.Infrastructure;
+
+namespace UrlsAndRoutes
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.Configure<RouteOptions>(options => options.ConstraintMap.Add("weekday", typeof(WeekDayConstraint)));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            app.UseStatusCodePages();
+            app.UseDeveloperExceptionPage();
+            app.UseStaticFiles();
+
+            //app.UseMvcWithDefaultRoute();
+            app.UseMvc(routes =>
+            {
+                routes.Routes.Add(new LegacyRoute(app.ApplicationServices, "/articles/Windows_3.1_Overview.html", "/old/.Net/"));
+                routes.MapRoute(
+                name: "areas",
+                template: "{area:exists}/{controller=Home}/{action=Index}");
+
+                routes.MapRoute(
+                 name: "NewRoute",
+                template: "App/Do{action}",
+                defaults: new { controller = "Home" });
+
+                routes.MapRoute(
+                name: "default",
+               template: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            //app.UseMvc(routes =>
+            //{
+            //    //routes.MapRoute(name: "ShopSchema2", template: "Shop/OldAction", defaults: new { controller = "Home", action = "Index" });
+            //    //routes.MapRoute(name: "ShopSchema", template: "Shop/{action}", defaults: new { controller = "Home" });
+            //    //routes.MapRoute("", "X{controller}/{action}");
+            //    //routes.MapRoute(name: "default", template: "{controller=Home}/{action=Index}");
+            //    //routes.MapRoute(name: "", template: "Public/{controller=Home}/{action=Index}");
+            //    routes.MapRoute(name: "MyRoute", template: "{controller=Home}/{action=Index}/{*catchAll}");
+            //});
+        }
+    }
+}
